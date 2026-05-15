6ff14248272ee479ac04da793029e8764403237f
Yutao Hou <98211687+hytup@users.noreply.github.com>
2026-05-15 20:59:35 +0800

    Update run-screen.sh

--- run-screen.sh
+++ run-screen.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 export WANDB_API_KEY="xxxxx"
-export OPENAI_API_BASE="http://0.0.0.0:xxxx/v1"
-export OPENAI_API_KEY="sk-xxxxx"
-export OPENAI_CLIENT_BASE_URL="http://0.0.0.0:xxxx/v1"
-export OPENAI_CLIENT_API_KEY="sk-xxxxx"
+export HOSTED_VLLM_API_BASE="http://0.0.0.0:xxxx/v1/"
+export TOGETHER_API_KEY="sk-xxxx"
+
+# # litellm_proxy/your-model-name
+export LITELLM_PROXY_API_BASE="http://0.0.0.0:xxxx/v1/"
+export LITELLM_PROXY_API_KEY="sk-xxxx"
+
 
 
 python screening.py --input_file "data/gsm8k.json"  --output_file "data/gsm8k-xxxxx-output.json" --model "xxx/model/xxxx"
